af01f912a8e7961d4d77f66f602ab2d065fabcbc
darkAngelCraft <alan_g@hotmail.it>
2017-03-31 15:41:46 +0200

    venerdi

--- main.py
+++ main.py
@@ -14,9 +14,16 @@ file = open("configured.txt", "r")
 configured = file.read()
 
 file2 = open("interface.txt", "r")
-interface = file.read()
+wlan = file2.read()
 
-print '0) set wlan_interface'
+if wlan == "null":
+    print "insert your wlan interface:"
+    wlan_interface=raw_input()
+    file2 = open("interface.txt", "w")
+    file2.write(wlan_interface)
+    sys.exit(0)
+
+
 print '1) start'
 print '2) reset'
 start = raw_input()
@@ -58,10 +65,7 @@ elif not start == '1':
 #non sono configurato
 if str(configured) == "null":
 
-    file2=open("interface.txt", "r")
-    wlan = file2.read()
-
-    print '\nconfiguration:'
+    print '\nconfiguration:(# '+wlan+' #)'
 
     print '1) router1 (192.168.1.1 - 1.1.1.1 - 3.3.3.1)'
     print '2) router2 (192.168.0.1 - 1.1.1.2 - 2.2.2.1)'
@@ -71,6 +75,8 @@ if str(configured) == "null":
 
     print '\n6) quagga OFF'
     print '7) quagga ON'
+    print '8) network OFF'
+    print '9) network ON'
 
     print '\nchoose configuration:'
     option = raw_input()
@@ -81,7 +87,7 @@ if str(configured) == "null":
     if option == '1':
         option = None
         #impostazione indirizzi IP
-        os.system('sudo ifconfig -v '+wlan+':1 192.168.1.1/24')
+        os.system('sudo ifconfig -v '+wlan+' 192.168.1.1/24')
         os.system('sudo ifconfig -v '+wlan+':2 1.1.1.1/24')
         os.system('sudo ifconfig -v '+wlan+':3 3.3.3.1/24')
 
@@ -89,7 +95,7 @@ if str(configured) == "null":
         os.system('sudo route del default')
 
         # aggiunge route per vedere le reti
-        os.system('sudo route add -net 192.168.1.0 netmask 255.255.255.0 gw 192.168.1.1 dev '+wlan+':1')
+        os.system('sudo route add -net 192.168.1.0 netmask 255.255.255.0 gw 192.168.1.1 dev '+wlan)
         os.system('sudo route add -net 1.1.1.0 netmask 255.255.255.0 gw 1.1.1.1 dev '+wlan+':2')
         os.system('sudo route add -net 3.3.3.0 netmask 255.255.255.0 gw 3.3.3.1 dev '+wlan+':3')
 
@@ -136,7 +142,7 @@ if str(configured) == "null":
     elif option == '2':
         option = None
         #impostazione indirizzi IP
-        os.system('sudo ifconfig -v '+wlan+':1 192.168.0.1/24')
+        os.system('sudo ifconfig -v '+wlan+' 192.168.0.1/24')
         os.system('sudo ifconfig -v '+wlan+':2 1.1.1.2/24')
         os.system('sudo ifconfig -v '+wlan+':3 2.2.2.1/24')
 
@@ -144,7 +150,7 @@ if str(configured) == "null":
         os.system('sudo route del default')
 
         # aggiunge route per vedere le reti
-        os.system('sudo route add -net 192.168.0.0 netmask 255.255.255.0 gw 192.168.0.1 dev '+wlan+':1')
+        os.system('sudo route add -net 192.168.0.0 netmask 255.255.255.0 gw 192.168.0.1 dev '+wlan)
         os.system('sudo route add -net 1.1.1.0 netmask 255.255.255.0 gw 1.1.1.2 dev '+wlan+':2')
         os.system('sudo route add -net 2.2.2.0 netmask 255.255.255.0 gw 2.2.2.1 dev '+wlan+':3')
 
@@ -192,14 +198,14 @@ if str(configured) == "null":
     elif option == '3':
         option = None
         #impostazione indirizzi IP
-        os.system('sudo ifconfig -v '+wlan+':1 3.3.3.2/24')
+        os.system('sudo ifconfig -v '+wlan+' 3.3.3.2/24')
         os.system('sudo ifconfig -v '+wlan+':2 2.2.2.2/24')
 
         # cancella le route di default
         os.system('sudo route del default')
 
         # aggiunge route per vedere le reti
-        os.system('sudo route add -net 3.3.3.0 netmask 255.255.255.0 gw 3.3.3.2 dev '+wlan+':1')
+        os.system('sudo route add -net 3.3.3.0 netmask 255.255.255.0 gw 3.3.3.2 dev '+wlan)
         os.system('sudo route add -net 2.2.2.0 netmask 255.255.255.0 gw 2.2.2.2 dev '+wlan+':2')
 
         # abilitare il forwarding dei pacchetti
@@ -302,6 +308,14 @@ if str(configured) == "null":
         os.system("sudo /etc/init.d/quagga restart")
         print "quagga service START!"
 
+    elif option == '8':
+        os.system('sudo ifconfig '+wlan+' off')
+        print wlan+" service OFF!"
+
+    elif option == '9':
+        os.system('sudo ifconfig ' + wlan + ' on')
+        print wlan+" service ON!"
+
     else:
         print '****************** reset configuration *************************'
         file = open("configured.txt", "w")
@@ -314,6 +328,10 @@ if str(configured) == "null":
 
 # gia configurato (configured = 1)
 else:
+
+    file2 = open("interface.txt", "r")
+    wlan = file2.read()
+
     int_option = None
 
     print '* you are a ' + str(configured) + ' *\n'
